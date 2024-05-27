599a7985505b3e057921559e0095f778ca248fe3
sanin6969 <145665810+sanin6969@users.noreply.github.com>
2024-05-27 10:08:17 +0530

    removed variance

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Cart,CartItem
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class cartAdmin(admin.ModelAdmin):
+    list_display=['cart_id','date_added']
+    
+class cartItemAdmin(admin.ModelAdmin):
+    list_display=['product','cart','quantity','is_active']
+admin.site.register(Cart,cartAdmin)
+admin.site.register(CartItem,cartItemAdmin)
